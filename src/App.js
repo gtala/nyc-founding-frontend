8875020fc9aa4e2df01984bebdf958636868ee27
CJ484 <ccoreas484@gmail.com>
2023-09-22 21:36:01 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.styles.scss';
-import { Languagelist } from './Components';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <Languagelist />
       <h1>New React App Template</h1>
     </div>
   );
